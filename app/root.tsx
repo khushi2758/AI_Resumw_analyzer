d2360e7eb09c46967e90ba0da291212e56a26387
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-10-26 20:40:58 +0530

    auth

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import type { Route } from "./+types/root";
-import "./app.css";
+import type { Route } from ".react-router/types/app/+types/root";
+import "~/app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
