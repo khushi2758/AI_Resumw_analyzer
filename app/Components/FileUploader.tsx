5b6492dacb7d9ab18700c245fdb8735270fd849c
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-12-02 23:42:34 +0530

    update

--- app/Components/FileUploader.tsx
+++ app/Components/FileUploader.tsx
@@ -1,20 +1,36 @@
-import React, {useCallback} from 'react'
+import React, { useCallback, useState } from 'react'
 import {useDropzone} from 'react-dropzone'
 
 function FileUploader() {
+    const [file, setFile] = useState()
     const onDrop = useCallback(acceptedFiles => {
         // Do something with the files
     }, [])
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
     return (
-        <div className="w-full gradient-border">
+        <div className="w-full gradient-border flex justify-center items-center">
             <div {...getRootProps()}>
                 <input {...getInputProps()} />
-                {
-                    isDragActive ?
-                        <p>Drop the files here ...</p> :
-                        <p>Drag 'n' drop some files here, or click to select files</p>
-                }
+                  <div className="space-y-4 cursor-pointer">
+                      <div className="mx-auto w-16 h-16 flex justify-center items-center">
+                      <img src="/icons/info.svg" alt="upload" className="size-20" />
+
+                      </div>
+                      {file? (
+                          <div>
+
+                          </div>
+                      ):(
+                          <div>
+                              <p className="text-lg text-gray-500">
+                          <span className="font-semibold">
+                              Click here to upload
+                          </span> or drop file
+                              </p>
+                              <p className ="text-lg text-gray-500" >PDF (max 20 MB) </p>
+                          </div>
+                      )}
+                  </div>
             </div>
 
         </div>
