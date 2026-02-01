a4f08c612e52167afe0f14144fe3db0b76696cc6
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2026-02-02 00:36:40 +0530

    fetch real data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,50 +1,78 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/Components/Navbar";
-import { resumes } from "../../constants";
 import ResumeCard from "~/Components/ResumeCard";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router";
-import { usePuterStore } from "~/lib/puter";
+import {usePuterStore} from "~/lib/puter";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ResuCraft" },
-    {
-      name: "Resume Analyzer",
-      content: "ResuCraft – Crafting perfect resumes with AI precision.",
-    },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-   const { auth} = usePuterStore();
-     const location = useLocation();
-    
-     const navigate = useNavigate();
-     useEffect(() => {
-        if (!auth.isAuthenticated) {
-            navigate('/auth?next=/' );
-        }
-},[auth.isAuthenticated]);
-  return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-      <Navbar />
-      <section className="main-section">
-        <div className="page-heading ">
-          <h1> Track Your Application & Resume Ratings</h1>
-          <h2>
-            Analyze your resume using AI, monitor your application progress, and
-            identify areas to boost your hiring potential.
-          </h2>
-        </div>
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
 
-        {resumes.length > 0 && (
-          <div className="resumes-section">
-            {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume} />
-            ))}
-          </div>
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+
+      const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+      const parsedResumes = resumes?.map((resume) => (
+          JSON.parse(resume.value) as Resume
+      ))
+
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
+    }
+
+    loadResumes()
+  }, []);
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get feedback.</h2>
+        ): (
+          <h2>Analyze your resume using AI, monitor your application progress, and
+            identify areas to boost your hiring potential.</h2>
         )}
-      </section>
-    </main>
-  );
+      </div>
+      {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+      )}
+
+      {!loadingResumes && resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+
+      {!loadingResumes && resumes?.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
+          </div>
+      )}
+    </section>
+  </main>
 }
